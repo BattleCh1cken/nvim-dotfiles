c1df8f2211064abe34141a810e40be726879895b
BattleCh1cken <75806385+BattleCh1cken@users.noreply.github.com>
2022-01-28 17:03:30 -0500

    Update colorscheme.lua

--- lua/user/colorscheme.lua
+++ lua/user/colorscheme.lua
@@ -1,1 +1,7 @@
-vim.cmd "colorscheme nord"
+local colorscheme = "nord"
+
+local status_ok, _ = pcall(vim.cmd, "colorscheme " .. colorscheme)
+if not status_ok then
+  vim.notify("colorscheme " .. colorscheme .. " not found!")
+  return
+end
