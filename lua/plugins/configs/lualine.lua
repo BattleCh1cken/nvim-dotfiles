f4f912eff4a780294d3633fff037380144bf97a8
* <BattleCh1cken>
2022-02-16 13:50:34 -0500

    added a new statusline

--- lua/plugins/configs/lualine.lua
+++ lua/plugins/configs/lualine.lua
@@ -3,30 +3,67 @@ if not ok then
 	return
 end
 
+
+
+
+-- Bubbles config for lualine
+-- Author: lokesh-krishna
+-- MIT license, see LICENSE for more details.
+
+-- stylua: ignore
+local colors = {
+  blue   = '#5E81AC',
+  cyan   = '#81A1C1',
+  black  = '#2E3440',
+  white  = '#c6c6c6',
+  red    = '#BF616A',
+  violet = '#B48EAD',
+  grey   = '#4C566A',
+}
+
+local bubbles_theme = {
+	normal = {
+		a = { fg = colors.black, bg = colors.violet },
+		b = { fg = colors.white, bg = colors.grey },
+		c = { fg = colors.black, bg = colors.black },
+	},
+
+	insert = { a = { fg = colors.black, bg = colors.blue } },
+	visual = { a = { fg = colors.black, bg = colors.cyan } },
+	replace = { a = { fg = colors.black, bg = colors.red } },
+
+	inactive = {
+		a = { fg = colors.white, bg = colors.black },
+		b = { fg = colors.white, bg = colors.black },
+		c = { fg = colors.black, bg = colors.black },
+	},
+}
+
 lualine.setup({
 	options = {
-		icons_enabled = true,
-		theme = "auto",
-		component_separators = { left = "", right = "" },
-		section_separators = { left = "", right = "" },
-		disabled_filetypes = { "NvimTree" },
-		always_divide_middle = true,
+		theme = bubbles_theme,
+		component_separators = "|",
+		section_separators = { left = "", right = "" },
 	},
 	sections = {
-		lualine_a = { "mode" },
-		lualine_b = { "branch", "diagnostics" },
-		lualine_c = { "filename" },
-		lualine_x = { "encoding", "filetype" },
-		lualine_y = { "progress" },
-		lualine_z = { "location" },
+		lualine_a = {
+			{ "mode", separator = { left = "" }, right_padding = 2 },
+		},
+		lualine_b = { "filename", "branch" },
+		lualine_c = { "fileformat" },
+		lualine_x = {},
+		lualine_y = { "filetype", "progress" },
+		lualine_z = {
+			{ "location", separator = { right = "" }, left_padding = 2 },
+		},
 	},
 	inactive_sections = {
-		lualine_a = {},
+		lualine_a = { "filename" },
 		lualine_b = {},
-		lualine_c = { "filename" },
-		lualine_x = { "location" },
+		lualine_c = {},
+		lualine_x = {},
 		lualine_y = {},
-		lualine_z = {},
+		lualine_z = { "location" },
 	},
 	tabline = {},
 	extensions = {},
